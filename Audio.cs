2930f720c38655a1a06caa4428fbc19155e4a0f3
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2022-08-11 23:19:14 +0100

    minor change to audio

--- Audio.cs
+++ Audio.cs
@@ -6,8 +6,8 @@ namespace ZFX {
         /// <summary>
         /// Play a certain Frequency
         /// </summary>
-        /// <param name="Frequency"></param>
-        /// <param name="Duration"></param>
+        /// <paramref name="Frequency">Frequency of the beep</paramref>
+        /// <paramref name="Duration">Duration of the beep</paramref>
         public void PlayFrequency(int Frequency, int Duration)
         {
             Console.Beep(Frequency, Duration);
